9d3994e72d759c47a1e2f918f098c920394941ba
Rendy Febry <rendyfebry@gmail.com>
2018-08-14 10:20:28 +0700

    Application loggin

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"strings"
 
-	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/log"
 	httptransport "github.com/go-kit/kit/transport/http"
 )
@@ -26,24 +25,18 @@ func (stringService) Count(_ context.Context, s string) int {
 func main() {
 	logger := log.NewLogfmtLogger(os.Stderr)
 
-	svc := stringService{}
-
-	var uppercase endpoint.Endpoint
-	uppercase = makeUppercaseEndpoint(svc)
-	uppercase = loggingMiddleware(log.With(logger, "method", "uppercase"))(uppercase)
-
-	var count endpoint.Endpoint
-	count = makeCountEndpoint(svc)
-	count = loggingMiddleware(log.With(logger, "method", "count"))(count)
+	var svc StringService
+	svc = stringService{}
+	svc = loggingMiddleware{logger, svc}
 
 	uppercaseHandler := httptransport.NewServer(
-		uppercase,
+		makeUppercaseEndpoint(svc),
 		decodeUppercaseRequest,
 		encodeResponse,
 	)
 
 	countHandler := httptransport.NewServer(
-		count,
+		makeCountEndpoint(svc),
 		decodeCountRequest,
 		encodeResponse,
 	)
